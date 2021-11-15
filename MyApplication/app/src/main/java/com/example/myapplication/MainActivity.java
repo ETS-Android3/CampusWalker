052fdd3bf86e279012a38955530c90b1cbb139fa
David Pittman <53283971+dopittman@users.noreply.github.com>
2021-11-14 20:30:44 -0500

    Add stackable emergency button

--- MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/MainActivity.java
@@ -13,6 +13,8 @@ import com.android.volley.Response;
 import com.android.volley.VolleyError;
 import com.android.volley.toolbox.StringRequest;
 import com.android.volley.toolbox.Volley;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
+import com.google.android.material.snackbar.Snackbar;
 
 public class MainActivity extends AppCompatActivity {
     public static final String EXTRA_MESSAGE = "com.example.myfirstapp.MESSAGE";
@@ -21,7 +23,43 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        FloatingActionButton fab1 = (FloatingActionButton) findViewById(R.id.fab1);
+        FloatingActionButton fab2 = (FloatingActionButton) findViewById(R.id.fab2);
+        FloatingActionButton fab3 = (FloatingActionButton) findViewById(R.id.fab3);
+        fab.setOnClickListener(new View.OnClickListener() {
+            boolean isFABOpen = false;
+
+            @Override
+            public void onClick(View view) {
+                if(!isFABOpen){
+                    showFABMenu();
+                }else{
+                    closeFABMenu();
+                }
+            }
+            private void showFABMenu(){
+                isFABOpen=true;
+                fab1.animate().translationY(-getResources().getDimension(R.dimen.standard_55));
+                fab2.animate().translationY(-getResources().getDimension(R.dimen.standard_105));
+                fab3.animate().translationY(-getResources().getDimension(R.dimen.standard_155));
+            }
+
+            private void closeFABMenu(){
+                isFABOpen=false;
+                fab1.animate().translationY(0);
+                fab2.animate().translationY(0);
+                fab3.animate().translationY(0);
+            }
+
+        });
+
+
+        
     }
+
+
 
     // Called when the user taps the Send button
     public void sendMessage(View view) {
@@ -39,9 +77,5 @@ public class MainActivity extends AppCompatActivity {
 //        intent.putExtra(EXTRA_MESSAGE, message);
         startActivity(intent);
     }
-
-//    public void openGuestActivity(View view){
-//        Intent intent = new Intent(this, HomeScreenActivity.class);
-//    }
 
 }
