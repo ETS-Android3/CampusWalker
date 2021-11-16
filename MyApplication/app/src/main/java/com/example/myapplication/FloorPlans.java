2cc71b5e0f195bd1d4a148ed69b14f5ef4986449
David Pittman <53283971+dopittman@users.noreply.github.com>
2021-11-16 10:38:03 -0500

    Add github scrolling library

--- MyApplication/app/src/main/java/com/example/myapplication/FloorPlans.java
+++ MyApplication/app/src/main/java/com/example/myapplication/FloorPlans.java
@@ -1,5 +1,6 @@
 package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.compose.ui.graphics.Canvas;
 
 import android.app.Activity;
 import android.media.Image;
@@ -11,6 +12,9 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.Spinner;
 
+import com.davemorrissey.labs.subscaleview.ImageSource;
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
+
 public class FloorPlans extends AppCompatActivity {
 
     ImageView iv;
@@ -21,7 +25,14 @@ public class FloorPlans extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_floor_plans);
-        iv = (ImageView) findViewById(R.id.floorImage);
+//        iv = (ImageView) findViewById(R.id.floorImage);
+
+        SubsamplingScaleImageView imageView = (SubsamplingScaleImageView)findViewById(R.id.imageView);
+        imageView.setImage(ImageSource.resource(R.drawable.oxendine_floor_one));
+
+
+
+
 
 
 
@@ -36,35 +47,35 @@ public class FloorPlans extends AppCompatActivity {
         // Apply the adapter to the spinner
                 spinner.setAdapter(adapter);
 
-        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Object selection = parent.getItemAtPosition(position);
-                String stringSelection = selection.toString();
-
-                System.out.println(stringSelection);
-
-                // Change to a switch statement when possible
-                if(stringSelection.equals("First Floor")){
-                    iv.setImageResource(images[0]);
-                    System.out.println("Floor 1");
-                } else if(stringSelection.equals("Second Floor")){
-                    iv.setImageResource(images[1]);
-                    System.out.println("Floor 2");
-                } else if(stringSelection.equals("Third Floor")){
-                    iv.setImageResource(images[2]);
-                    System.out.println("Floor 3");
-                } else {
-                    System.out.println("Selection did not match");
-                }
-            }
-
-            @Override
-            // Use if necessary
-            public void onNothingSelected(AdapterView<?> parent) {
-                // Intentionally kept blank
-            }
-        });
+//        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+//            @Override
+//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+//                Object selection = parent.getItemAtPosition(position);
+//                String stringSelection = selection.toString();
+//
+//                System.out.println(stringSelection);
+//
+//                // Change to a switch statement when possible
+//                if(stringSelection.equals("First Floor")){
+//                    iv.setImageResource(images[0]);
+//                    System.out.println("Floor 1");
+//                } else if(stringSelection.equals("Second Floor")){
+//                    iv.setImageResource(images[1]);
+//                    System.out.println("Floor 2");
+//                } else if(stringSelection.equals("Third Floor")){
+//                    iv.setImageResource(images[2]);
+//                    System.out.println("Floor 3");
+//                } else {
+//                    System.out.println("Selection did not match");
+//                }
+//            }
+//
+//            @Override
+//            // Use if necessary
+//            public void onNothingSelected(AdapterView<?> parent) {
+//                // Intentionally kept blank
+//            }
+//        });
 
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> roomNumAdapter = ArrayAdapter.createFromResource(this,
@@ -108,6 +119,8 @@ public class FloorPlans extends AppCompatActivity {
 
 
     }
+
+
 
 
 }
