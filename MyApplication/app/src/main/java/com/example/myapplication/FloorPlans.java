564f9e19d1f66c59a559f7fc48f9190c8e423b97
David Pittman <53283971+dopittman@users.noreply.github.com>
2021-11-18 06:09:51 -0500

    Add scalability, double tap zoom, and reduce lag in FloorPlans activity

--- MyApplication/app/src/main/java/com/example/myapplication/FloorPlans.java
+++ MyApplication/app/src/main/java/com/example/myapplication/FloorPlans.java
@@ -29,6 +29,9 @@ public class FloorPlans extends AppCompatActivity {
 
         SubsamplingScaleImageView imageView = (SubsamplingScaleImageView)findViewById(R.id.imageView);
         imageView.setImage(ImageSource.resource(R.drawable.oxendine_floor_one));
+        imageView.setMinimumDpi(160);
+        imageView.setDoubleTapZoomDpi(160);
+        imageView.setMinimumTileDpi(160);
 
 
 
@@ -47,35 +50,35 @@ public class FloorPlans extends AppCompatActivity {
         // Apply the adapter to the spinner
                 spinner.setAdapter(adapter);
 
-//        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
-//            @Override
-//            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-//                Object selection = parent.getItemAtPosition(position);
-//                String stringSelection = selection.toString();
-//
-//                System.out.println(stringSelection);
-//
-//                // Change to a switch statement when possible
-//                if(stringSelection.equals("First Floor")){
-//                    iv.setImageResource(images[0]);
-//                    System.out.println("Floor 1");
-//                } else if(stringSelection.equals("Second Floor")){
-//                    iv.setImageResource(images[1]);
-//                    System.out.println("Floor 2");
-//                } else if(stringSelection.equals("Third Floor")){
-//                    iv.setImageResource(images[2]);
-//                    System.out.println("Floor 3");
-//                } else {
-//                    System.out.println("Selection did not match");
-//                }
-//            }
-//
-//            @Override
-//            // Use if necessary
-//            public void onNothingSelected(AdapterView<?> parent) {
-//                // Intentionally kept blank
-//            }
-//        });
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                Object selection = parent.getItemAtPosition(position);
+                String stringSelection = selection.toString();
+
+                System.out.println(stringSelection);
+
+                // Change to a switch statement when possible
+                if(stringSelection.equals("First Floor")){
+                    imageView.setImage(ImageSource.resource(R.drawable.oxendine_floor_one));
+                    System.out.println("Floor 1");
+                } else if(stringSelection.equals("Second Floor")){
+                    imageView.setImage(ImageSource.resource(R.drawable.oxendine_floor_two));
+                    System.out.println("Floor 2");
+                } else if(stringSelection.equals("Third Floor")){
+                    imageView.setImage(ImageSource.resource(R.drawable.oxendine_floor_three));
+                    System.out.println("Floor 3");
+                } else {
+                    System.out.println("Selection did not match");
+                }
+            }
+
+            @Override
+            // Use if necessary
+            public void onNothingSelected(AdapterView<?> parent) {
+                // Intentionally kept blank
+            }
+        });
 
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> roomNumAdapter = ArrayAdapter.createFromResource(this,
